fbae7a3b00bc3579a2b57ed888f840c9c7eef3c5
1035040175@qq.com <1035041075@qq.com>
2017-08-23 17:59:02 +0800

    修改了webpack 的build

--- config/index.js
+++ config/index.js
@@ -7,13 +7,13 @@ module.exports = {
     index: path.resolve(__dirname, '../server/public/index.html'),
     assetsRoot: path.resolve(__dirname, '../server/public'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '',
+    assetsPublicPath: 'http://SSnewbie.coding.me/Test',
   },
   dev: {
     env: require('./dev.env'),
     port: 8082,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
   }
 }
